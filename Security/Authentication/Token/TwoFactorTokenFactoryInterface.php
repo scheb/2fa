11b61aebe84e923b42a51cbc7d9a288544cb7298
Christian Scheb <info@stylemotion.de>
2018-10-09 23:12:39 +0200

    Add interface documentation

--- Security/Authentication/Token/TwoFactorTokenFactoryInterface.php
+++ Security/Authentication/Token/TwoFactorTokenFactoryInterface.php
@@ -6,5 +6,15 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 interface TwoFactorTokenFactoryInterface
 {
+    /**
+     * Create a new TwoFactorToken
+     *
+     * @param TokenInterface $authenticatedToken
+     * @param null|string $credentials The two-factor authentication code or null
+     * @param string $providerKey The firewall name
+     * @param string[] $twoFactorProviders The two-factor provider aliases, which are currently available
+     *
+     * @return TwoFactorTokenInterface
+     */
     public function create(TokenInterface $authenticatedToken, ?string $credentials, string $providerKey, array $twoFactorProviders): TwoFactorTokenInterface;
 }
