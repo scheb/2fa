d5d526bdcad37811960f43b7cab26505861ceb38
Christian Scheb <info@stylemotion.de>
2018-11-12 22:37:10 +0100

    Add missing PHPDoc

--- Security/TwoFactor/IpWhitelist/IpWhitelistProviderInterface.php
+++ Security/TwoFactor/IpWhitelist/IpWhitelistProviderInterface.php
@@ -10,6 +10,8 @@ interface IpWhitelistProviderInterface
     /**
      * Return a list of whitelisted IP addresses, which don't need to perform two-factor authentication.
      *
+     * @param AuthenticationContextInterface $context
+     *
      * @return string[]
      */
     public function getWhitelistedIps(AuthenticationContextInterface $context): array;
