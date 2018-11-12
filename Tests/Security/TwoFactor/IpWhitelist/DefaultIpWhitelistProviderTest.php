53c706721f430feff6dcce06c392ab4289bb88eb
Christian Scheb <info@stylemotion.de>
2018-11-12 22:40:47 +0100

    Use mocked AuthenticationContextInterface in the test case

--- Tests/Security/TwoFactor/IpWhitelist/DefaultIpWhitelistProviderTest.php
+++ Tests/Security/TwoFactor/IpWhitelist/DefaultIpWhitelistProviderTest.php
@@ -2,8 +2,8 @@
 
 namespace Scheb\TwoFactorBundle\Tests\Security\TwoFactor\IpWhitelist;
 
-use Scheb\TwoFactorBundle\Security\TwoFactor\AuthenticationContext;
-use Scheb\TwoFactorBundle\Security\TwoFactor\AuthenticationContextFactory;
+use PHPUnit\Framework\MockObject\MockObject;
+use Scheb\TwoFactorBundle\Security\TwoFactor\AuthenticationContextInterface;
 use Scheb\TwoFactorBundle\Security\TwoFactor\IpWhitelist\DefaultIpWhitelistProvider;
 use Scheb\TwoFactorBundle\Tests\TestCase;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,16 +21,10 @@ class DefaultIpWhitelistProviderTest extends TestCase
      */
     private $token;
 
-    /**
-     * @var AuthenticationContextFactory
-     */
-    private $authenticationContextFactory;
-
     protected function setUp()
     {
         $this->request = $this->createMock(Request::class);
         $this->token = $this->createMock(TokenInterface::class);
-        $this->authenticationContextFactory = new AuthenticationContextFactory(AuthenticationContext::class);
     }
 
     /**
@@ -39,7 +33,7 @@ class DefaultIpWhitelistProviderTest extends TestCase
     public function testGetWhitelistedIps_hasIpsConfigured_returnThoseIps(): void
     {
         $ipWhitelist = ['1.0.0.0', '2.0.0.0'];
-        $context = $this->authenticationContextFactory->create($this->request, $this->token, 'firewallName');
+        $context = $this->createMock(AuthenticationContextInterface::class);
         $provider = new DefaultIpWhitelistProvider($ipWhitelist);
         $this->assertEquals($ipWhitelist, $provider->getWhitelistedIps($context));
     }
