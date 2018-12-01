410d2b7dee90bdbeb4653fd188bda1d47e512938
Marcel Marnitz <marcelmarnitz@googlemail.com>
2018-12-01 21:40:55 +0100

    Fixes Symfony 4.2 deprecation warning about TreeBuilder::_construct() calling without a parameter

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,8 +9,14 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('scheb_two_factor');
+        $treeBuilder = new TreeBuilder('scheb_two_factor');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('scheb_two_factor');
+        }
 
         $rootNode
             ->children()
