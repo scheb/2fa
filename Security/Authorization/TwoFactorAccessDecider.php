6e23a3f7478d29d599dbb5e7fa0551d2130d688f
Christian Scheb <privat@stylemotion.de>
2019-05-03 14:34:37 +0200

    Add a check to always let the logout route pass

--- Security/Authorization/TwoFactorAccessDecider.php
+++ Security/Authorization/TwoFactorAccessDecider.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
 use Symfony\Component\Security\Http\AccessMapInterface;
+use Symfony\Component\Security\Http\HttpUtils;
+use Symfony\Component\Security\Http\Logout\LogoutUrlGenerator;
 
 class TwoFactorAccessDecider
 {
@@ -21,12 +23,26 @@ class TwoFactorAccessDecider
      */
     private $accessDecisionManager;
 
+    /**
+     * @var HttpUtils
+     */
+    private $httpUtils;
+
+    /**
+     * @var LogoutUrlGenerator
+     */
+    private $logoutUrlGenerator;
+
     public function __construct(
         AccessMapInterface $accessMap,
-        AccessDecisionManagerInterface $accessDecisionManager
+        AccessDecisionManagerInterface $accessDecisionManager,
+        HttpUtils $httpUtils,
+        LogoutUrlGenerator $logoutUrlGenerator
     ) {
         $this->accessMap = $accessMap;
         $this->accessDecisionManager = $accessDecisionManager;
+        $this->httpUtils = $httpUtils;
+        $this->logoutUrlGenerator = $logoutUrlGenerator;
     }
 
     public function isAccessible(Request $request, TokenInterface $token): bool
@@ -34,6 +50,12 @@ class TwoFactorAccessDecider
         // Let routes pass, e.g. if a route needs to be callable during two-factor authentication
         list($attributes) = $this->accessMap->getPatterns($request);
         if (null !== $attributes && $this->accessDecisionManager->decide($token, $attributes, $request)) {
+            return true;
+        }
+
+        // Let the logout route pass
+        $logoutPath = $this->logoutUrlGenerator->getLogoutPath();
+        if ($this->httpUtils->checkRequestPath($request, $logoutPath)) {
             return true;
         }
 
--- Tests/Security/Authorization/TwoFactorAccessDeciderTest.php
+++ Tests/Security/Authorization/TwoFactorAccessDeciderTest.php
@@ -12,9 +12,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
 use Symfony\Component\Security\Http\AccessMapInterface;
+use Symfony\Component\Security\Http\HttpUtils;
+use Symfony\Component\Security\Http\Logout\LogoutUrlGenerator;
 
 class TwoFactorAccessDeciderTest extends TestCase
 {
+    private const LOGOUT_PATH = '/logout';
+
     /**
      * @var MockObject|Request
      */
@@ -36,6 +40,16 @@ class TwoFactorAccessDeciderTest extends TestCase
     private $accessDecisionManager;
 
     /**
+     * @var MockObject|HttpUtils
+     */
+    private $httpUtils;
+
+    /**
+     * @var MockObject|LogoutUrlGenerator
+     */
+    private $logoutUrlGenerator;
+
+    /**
      * @var TwoFactorAccessDecider
      */
     private $accessDecider;
@@ -46,6 +60,8 @@ class TwoFactorAccessDeciderTest extends TestCase
         $this->token = $this->createMock(TokenInterface::class);
         $this->accessMap = $this->createMock(AccessMapInterface::class);
         $this->accessDecisionManager = $this->createMock(AccessDecisionManagerInterface::class);
+        $this->httpUtils = $this->createMock(HttpUtils::class);
+        $this->logoutUrlGenerator = $this->createMock(LogoutUrlGenerator::class);
 
         // Stub an access rule
         $this->accessMap
@@ -54,10 +70,16 @@ class TwoFactorAccessDeciderTest extends TestCase
             ->with($this->request)
             ->willReturn([[TwoFactorInProgressVoter::IS_AUTHENTICATED_2FA_IN_PROGRESS], 'https']);
 
-        $this->accessDecider = new TwoFactorAccessDecider($this->accessMap, $this->accessDecisionManager);
+        // Stub the logout path
+        $this->logoutUrlGenerator
+            ->expects($this->any())
+            ->method('getLogoutPath')
+            ->willReturn(self::LOGOUT_PATH);
+
+        $this->accessDecider = new TwoFactorAccessDecider($this->accessMap, $this->accessDecisionManager, $this->httpUtils, $this->logoutUrlGenerator);
     }
 
-    private function whenPathAccessIsChecked(bool $accessGranted): void
+    private function whenPathAccess(bool $accessGranted): void
     {
         $this->accessDecisionManager
             ->expects($this->any())
@@ -66,12 +88,23 @@ class TwoFactorAccessDeciderTest extends TestCase
             ->willReturn($accessGranted);
     }
 
+    private function whenIsLogoutPath(bool $accessGranted): void
+    {
+        $this->httpUtils
+            ->expects($this->any())
+            ->method('checkRequestPath')
+            ->with($this->request, self::LOGOUT_PATH)
+            ->willReturn($accessGranted);
+    }
+
     /**
      * @test
      */
     public function isAccessible_pathAccessGranted_returnTrue(): void
     {
-        $this->whenPathAccessIsChecked(true);
+        $this->whenPathAccess(true);
+        $this->whenIsLogoutPath(false);
+
         $returnValue = $this->accessDecider->isAccessible($this->request, $this->token);
         $this->assertTrue($returnValue);
     }
@@ -79,9 +112,23 @@ class TwoFactorAccessDeciderTest extends TestCase
     /**
      * @test
      */
-    public function isAccessible_pathAccessDenied_returnFalse(): void
+    public function isAccessible_isLogoutPath_returnTrue(): void
     {
-        $this->whenPathAccessIsChecked(false);
+        $this->whenPathAccess(false);
+        $this->whenIsLogoutPath(true);
+
+        $returnValue = $this->accessDecider->isAccessible($this->request, $this->token);
+        $this->assertTrue($returnValue);
+    }
+
+    /**
+     * @test
+     */
+    public function isAccessible_isNotAccessible_returnFalse(): void
+    {
+        $this->whenPathAccess(false);
+        $this->whenIsLogoutPath(false);
+
         $returnValue = $this->accessDecider->isAccessible($this->request, $this->token);
         $this->assertFalse($returnValue);
     }
