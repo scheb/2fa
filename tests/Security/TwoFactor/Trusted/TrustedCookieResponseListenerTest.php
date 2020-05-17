scheb/two-factor-bundle

Add missing type hints, fix method name

Tests/Security/TwoFactor/Trusted/TrustedCookieResponseListenerTest.php
@@ -71,7 +71,7 @@ class TrustedCookieResponseListenerTest extends TestCase
     /**
      * @return MockObject|FilterResponseEvent|RequestEvent
      */
-    private function createEventWithRequest(MockObject $request)
+    private function createEventWithRequest(MockObject $request): MockObject
     {
         // Symfony < 4.3
         if (!class_exists(RequestEvent::class)) {

scheb/2fa

tests/Security/TwoFactor/Trusted/TrustedCookieResponseListenerTest.php
@@ -70,10 +70,8 @@ class TrustedCookieResponseListenerTest extends TestCase
 
     /**
      * @param MockObject|Request $request
-     *
-     * @return MockObject|ResponseEvent
      */
-    private function createEventWithRequest(MockObject $request)
+    private function createEventWithRequest(MockObject $request): ResponseEvent
     {
         return new ResponseEvent(
             $this->createMock(HttpKernelInterface::class),