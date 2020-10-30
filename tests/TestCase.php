a08ecbd204e1530255570f7f42a4b0d0c86cc1e6
Christian Scheb <privat@stylemotion.de>
2020-10-30 20:13:44 +0100

    Code style fix

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -20,7 +20,7 @@ abstract class TestCase extends PHPUnitTestCase
     private function requireAtLeastSymfonyVersion(int $version)
     {
         if (Kernel::VERSION_ID < $version) {
-            $this->markTestSkipped("Skipping test case, minimum required Symfony version not fulfilled.");
+            $this->markTestSkipped('Skipping test case, minimum required Symfony version not fulfilled.');
         }
     }
 }
