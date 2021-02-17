81da4c9fc8e9aa0593220c1e5c1f2f256b4ffffb
Julian Sawicki <jsawicki@internetcompany.nl>
2021-02-17 11:16:26 +0100

    Improvements after code review by Christian Scheb

--- src/bundle/Security/Http/Authenticator/Passport/TwoFactorPassport.php
+++ src/bundle/Security/Http/Authenticator/Passport/TwoFactorPassport.php
@@ -8,14 +8,13 @@ use RuntimeException;
 use Scheb\TwoFactorBundle\Security\Authentication\Token\TwoFactorTokenInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CredentialsInterface;
-use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportTrait;
 use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
 
 /**
  * @final
  */
-class TwoFactorPassport implements PassportInterface, UserPassportInterface
+class TwoFactorPassport implements UserPassportInterface
 {
     use PassportTrait;
 
@@ -46,7 +45,7 @@ class TwoFactorPassport implements PassportInterface, UserPassportInterface
     public function getUser(): UserInterface
     {
         $user = $this->twoFactorToken->getUser();
-        if($user instanceof UserInterface) {
+        if ($user instanceof UserInterface) {
             return $user;
         }
         throw new RuntimeException('Failed to find User of type UserInterface');
