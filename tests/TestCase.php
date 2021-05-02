3bf87aec69cf174bddc2452279d2fdf2f7833794
Christian Scheb <privat@stylemotion.de>
2021-05-02 14:53:13 +0200

    Add missing return type hints

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,12 +15,12 @@ abstract class TestCase extends PHPUnitTestCase
 {
     private const SYMFONY_5_3 = 50300;
 
-    protected function requireAtLeastSymfony5_3()
+    protected function requireAtLeastSymfony5_3(): void
     {
         $this->requireAtLeastSymfonyVersion(self::SYMFONY_5_3);
     }
 
-    private function requireAtLeastSymfonyVersion(int $version)
+    private function requireAtLeastSymfonyVersion(int $version): void
     {
         if (Kernel::VERSION_ID < $version) {
             $this->markTestSkipped('Skipping test case, minimum required Symfony version not fulfilled.');
