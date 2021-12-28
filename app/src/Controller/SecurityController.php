6967ca38d0a9066b55acf81548dd13a16e7d8808
Christian Scheb <privat@stylemotion.de>
2021-12-28 15:21:52 +0100

    Migrate to UserPasswordHasherInterface

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -7,8 +7,8 @@ namespace App\Controller;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -33,11 +33,11 @@ class SecurityController extends AbstractController
     /**
      * @Route("/password", name="_security_password")
      */
-    public function register(UserPasswordEncoderInterface $encoder)
+    public function register(UserPasswordHasherInterface $hasher)
     {
         $user = new User();
         $plainPassword = 'test';
-        $encoded = $encoder->encodePassword($user, $plainPassword);
+        $encoded = $hasher->hashPassword($user, $plainPassword);
 
         return new Response($encoded);
     }
