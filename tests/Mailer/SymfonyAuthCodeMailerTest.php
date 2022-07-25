db3dec7d5511a250d3c13f8208c83482ee81d1d3
Alexander Schranz <alexander@sulu.io>
2022-07-25 18:25:49 +0200

    Allow to use symfony mailer default configuration (#153)
    Allow to use symfony mailer default configuration (#153)

--- tests/Mailer/SymfonyAuthCodeMailerTest.php
+++ tests/Mailer/SymfonyAuthCodeMailerTest.php
@@ -62,6 +62,46 @@ class SymfonyAuthCodeMailerTest extends TestCase
     /**
      * @test
      */
+    public function sendAuthCode_hasAuthCode_sendEmailWithoutSender(): void
+    {
+        $mailer = new SymfonyAuthCodeMailer($this->symfonyMailer, null, null);
+
+        // Stub the user object
+        $user = $this->createMock(TwoFactorInterface::class);
+        $user
+            ->expects($this->any())
+            ->method('getEmailAuthRecipient')
+            ->willReturn('recipient@example.com');
+        $user
+            ->expects($this->any())
+            ->method('getEmailAuthCode')
+            ->willReturn('1234');
+
+        $messageValidator = function (Email $mail) {
+            // normally set by the mailer in a listener https://github.com/symfony/mailer/blob/8fa150355115ea09238858ae3cfaf249fd1fd5ed/EventListener/EnvelopeListener.php#L49
+            // can be removed when min version is symfony 6.1 where sender is to check the message
+            $mail->sender('no-reply@example.com');
+
+            $this->assertEquals('recipient@example.com', current($mail->getTo())->getAddress());
+            $this->assertFalse(current($mail->getFrom()));
+            $this->assertEquals('Authentication Code', $mail->getSubject());
+            $this->assertEquals('1234', $mail->getBody()->bodyToString());
+
+            return true;
+        };
+
+        // Expect mail to be sent
+        $this->symfonyMailer
+            ->expects($this->once())
+            ->method('send')
+            ->with($this->callback($messageValidator));
+
+        $mailer->sendAuthCode($user);
+    }
+
+    /**
+     * @test
+     */
     public function sendAuthCode_nullAuthCode_sendEmail(): void
     {
         // Stub the user object
