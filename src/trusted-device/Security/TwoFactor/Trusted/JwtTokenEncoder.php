dbbdde96e81c956d6a5cadf02f49acfdf25b9011
Luís Cobucci <lcobucci@gmail.com>
2022-08-21 16:04:24 +0200

    Split default constraints from configuration object
    This prepares the implementation for allowing the injection of
    configuration objects, allowing users to override the configuration of
    the underlying library.
    
    It also allows the injection of clock implementations and replaces the
    logic to validate date claims in the token.
    
    Signed-off-by: Luís Cobucci <lcobucci@gmail.com>

--- src/trusted-device/Security/TwoFactor/Trusted/JwtTokenEncoder.php
+++ src/trusted-device/Security/TwoFactor/Trusted/JwtTokenEncoder.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Scheb\TwoFactorBundle\Security\TwoFactor\Trusted;
 
 use DateTimeImmutable;
+use Lcobucci\Clock\Clock;
+use Lcobucci\Clock\SystemClock;
 use Lcobucci\JWT\Configuration;
 use Lcobucci\JWT\Exception;
 use Lcobucci\JWT\Signer\Hmac\Sha256;
 use Lcobucci\JWT\Signer\Key\InMemory;
 use Lcobucci\JWT\Token\Plain;
-use Lcobucci\JWT\Validation\Constraint\SignedWith;
+use Lcobucci\JWT\Validation\Constraint;
 
 /**
  * @internal
@@ -24,17 +26,18 @@ class JwtTokenEncoder
     public const CLAIM_VERSION = 'vsn';
 
     private Configuration $configuration;
+    private Clock $clock;
 
-    public function __construct(string $applicationSecret)
+    public function __construct(string $applicationSecret, ?Clock $clock = null)
     {
         $this->configuration = Configuration::forSymmetricSigner(new Sha256(), InMemory::plainText($applicationSecret));
-        $this->configuration->setValidationConstraints(new SignedWith($this->configuration->signer(), $this->configuration->signingKey()));
+        $this->clock = $clock ?? SystemClock::fromSystemTimezone();
     }
 
     public function generateToken(string $username, string $firewallName, int $version, DateTimeImmutable $validUntil): Plain
     {
         $builder = $this->configuration->builder()
-            ->issuedAt(new DateTimeImmutable())
+            ->issuedAt($this->clock->now())
             ->expiresAt($validUntil)
             ->withClaim(self::CLAIM_USERNAME, $username)
             ->withClaim(self::CLAIM_FIREWALL, $firewallName)
@@ -55,14 +58,18 @@ class JwtTokenEncoder
             return null;
         }
 
-        if (!$this->configuration->validator()->validate($token, ...$this->configuration->validationConstraints())) {
-            return null;
-        }
-
-        if ($token->isExpired(new DateTimeImmutable())) {
+        if (!$this->configuration->validator()->validate($token, ...$this->validationConstraints())) {
             return null;
         }
 
         return $token;
+    }
+
+    /** @return iterable<int, Constraint> */
+    private function validationConstraints(): iterable
+    {
+        yield new Constraint\SignedWith($this->configuration->signer(), $this->configuration->signingKey());
+        yield new Constraint\ValidAt($this->clock); // replace with LooseValidAt once dependency on lcobucci/jwt is bumped up
+        yield from $this->configuration->validationConstraints();
     }
 }
