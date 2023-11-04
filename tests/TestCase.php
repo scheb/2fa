191a96c2ff6ccd7e30a6ceb19d065d64cd210124
Christian Scheb <privat@stylemotion.de>
2023-11-04 12:52:47 +0100

    Remove infrastructure to manage test cases on different Symfony versions

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,35 +10,4 @@ use Symfony\Component\HttpKernel\Kernel;
 // phpcs:ignore Symfony.NamingConventions.ValidClassName
 abstract class TestCase extends PHPUnitTestCase
 {
-    private const MINOR_VERSION_INCREMENT = 100;
-    private const SYMFONY_5_4 = 50400;
-    private const SYMFONY_6_0 = 60000;
-
-    protected function requireAtMostSymfony5_4(): void
-    {
-        $this->requireAtMostSymfonyVersion(self::SYMFONY_5_4);
-    }
-
-    private function requireAtMostSymfonyVersion(int $version): void
-    {
-        if (Kernel::VERSION_ID < $version + self::MINOR_VERSION_INCREMENT) {
-            return;
-        }
-
-        $this->markTestSkipped('Skipping test case, minimum required Symfony version not fulfilled.');
-    }
-
-    protected function requireAtLeastSymfony6_0(): void
-    {
-        $this->requireAtLeastSymfonyVersion(self::SYMFONY_6_0);
-    }
-
-    private function requireAtLeastSymfonyVersion(int $version): void
-    {
-        if (Kernel::VERSION_ID >= $version) {
-            return;
-        }
-
-        $this->markTestSkipped('Skipping test case, minimum required Symfony version not fulfilled.');
-    }
 }
