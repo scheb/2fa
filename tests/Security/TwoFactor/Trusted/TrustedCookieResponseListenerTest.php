9859c17402f01fa77113ccdedb1041a948846792
Christian Scheb <privat@stylemotion.de>
2025-11-20 12:48:59 +0100

    Fix providing IPv6 for testing

--- tests/Security/TwoFactor/Trusted/TrustedCookieResponseListenerTest.php
+++ tests/Security/TwoFactor/Trusted/TrustedCookieResponseListenerTest.php
@@ -175,7 +175,7 @@ class TrustedCookieResponseListenerTest extends TestCase
         return [
             ['localhost'],
             ['123.0.0.1'],
-            ['2001:0db8:85a3:0000:0000:8a2e:0370:7334'],
+            ['[2001:0db8:85a3:0000:0000:8a2e:0370:7334]'],
         ];
     }
 
